d5ba20bd5b15a56f02bf20e8c081f4b8aedd5b06
ragnaringi <ragnar.hrafnkelsson@gmail.com>
2018-05-22 22:48:04 +0100

    [hv-projectgenerator] refactor

--- hv-projectgenerator/__main__.py
+++ hv-projectgenerator/__main__.py
@@ -7,6 +7,13 @@ from distutils.dir_util import copy_tree
 
 binDir = os.path.join(sys.path[0], "bin")
 
+def copy(srcPath, dstPath):
+  if os.path.isdir(srcPath):
+    copy_tree(srcPath, dstPath)
+  else:
+    shutil.copy2(srcPath, dstPath)
+
+
 def getScriptPath():
   return os.path.dirname(os.path.realpath(sys.argv[0]))
 
@@ -16,37 +23,6 @@ def getFilesWithExtension(srcDir, extensions):
   for ext in extensions:
     files.extend(glob(join(srcDir, ext)))
   return files
-
-
-# Extracts library name from a Heavy source directory
-def getLibraryName(srcDir):
-  file = [filename for filename in os.listdir(srcDir) if filename.startswith("Heavy_")][0]
-  return (os.path.splitext(file)[0])
-
-
-def copyProjectFiles(dstDir, projectType):
-  projectPath = join(binDir, sys.platform, projectType)
-  copy_tree(projectPath, dstDir)
-
-
-def renameProject(projectPath, name):
-  if sys.platform == "darwin":
-    newPath = join(path.dirname(projectPath), name + ".xcodeproj")
-    if path.exists(newPath):
-      shutil.rmtree(newPath)
-    os.rename(projectPath, newPath)
-  else:
-    raise NotImplementedError
-  
-  return newPath
-
-
-def updateProjectFileReferences(projectPath, name):
-  if sys.platform == "darwin":
-    filePath = join(projectPath, "project.pbxproj")
-    replaceOccurencesOfStringInFile(filePath, "test", name)
-  else:
-    raise NotImplementedError
 
 
 def replaceOccurencesOfStringInFile(filePath, string, replacement):
@@ -61,20 +37,66 @@ def replaceOccurencesOfStringInFile(filePath, string, replacement):
   os.remove(tempPath)
 
 
+# Extracts library name from a Heavy source directory
+def getLibraryName(srcDir):
+  file = [filename for filename in os.listdir(srcDir) if filename.startswith("Heavy_")][0]
+  return (os.path.splitext(file)[0])
+
+
+def copyProjectFiles(dstDir, projectType):
+  filePaths = []
+  srcDir = join(binDir, sys.platform, projectType)
+  files = os.listdir(srcDir)
+  for file in files:
+    if file == ".DS_Store": continue
+    oldPath = join(srcDir,file)
+    newPath = join(dstDir, file)
+    copy(oldPath, newPath)
+    filePaths.append(newPath)
+  return filePaths
+
+
+def renameProject(projectFiles, name):
+  if not sys.platform == "darwin":
+    raise NotImplementedError
+  
+  filePaths = []
+
+  for file in projectFiles:
+    newPath = file
+    extension = os.path.splitext(file)[1]
+    if extension == ".xcodeproj":
+      newPath = join(path.dirname(file), name + extension)
+      if path.exists(newPath):
+        shutil.rmtree(newPath)
+      os.rename(file, newPath)
+
+    filePaths.append(newPath)
+  
+  return filePaths
+
+
+def updateProjectFileReferences(projectFiles, name):
+  if not sys.platform == "darwin":
+    raise NotImplementedError
+  
+  for file in projectFiles:
+    newPath = file
+    if os.path.splitext(file)[1] == ".xcodeproj":
+      newPath = join(file, "project.pbxproj")
+    replaceOccurencesOfStringInFile(newPath, "test", name)
+
+
 def createProject(srcDir, name, projectType):
   try:
-    # Copy project
-    copyProjectFiles(srcDir, projectType)
+    # Copy project files
+    projectFiles = copyProjectFiles(srcDir, projectType)
 
     # Rename project
-    projectPath = renameProject(join(srcDir, projectType + ".xcodeproj"), name)
+    projectFiles = renameProject(projectFiles, name)
 
-    # Edit project file references
-    updateProjectFileReferences(projectPath, name)
-
-    # Edit source files references
-    mainFile = join(srcDir, "main." + projectType)
-    replaceOccurencesOfStringInFile(mainFile, "test", name)
+    # Edit file references references
+    updateProjectFileReferences(projectFiles, name)
 
   finally:
     print("Project Created")
@@ -93,6 +115,7 @@ if __name__ == '__main__':
   #    ask to user to select if multiple are present
   # 2. Check for project type. Currently only Xcode
   #    command line C is supported
+  # 3. Add option to precompile static lib
   #
   if not (sys.platform == "darwin"):
     print("Currently only supported on Mac OSX")
