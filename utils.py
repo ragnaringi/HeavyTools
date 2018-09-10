c7fb742b031d070042fd722f311ba5b733e101e8
ragnaringi <ragnar.hrafnkelsson@gmail.com>
2018-09-10 15:29:03 +0100

    Adds functions to utils.py

--- utils.py
+++ utils.py
@@ -12,3 +12,19 @@ def currentFilePath():
 
 def currentFileDirectory():
     return os.path.dirname(__file__)
+
+def replaceOccurencesOfStringInFile(filePath, string, replacement):
+    tempPath = os.path.join(os.path.dirname(filePath), "temp" + os.path.splitext(filePath)[1])
+    os.rename(filePath, tempPath)
+    
+    with open(tempPath, "rt") as fin:
+        with open(filePath, "wt") as fout:
+            for line in fin:
+                fout.write(line.replace(string, replacement))
+                
+    os.remove(tempPath)
+
+# Extracts library name from a Heavy source directory
+def getLibraryName(srcDir):
+    file = [filename for filename in os.listdir(srcDir) if filename.startswith("Heavy_")][0]
+    return (os.path.splitext(file)[0])
